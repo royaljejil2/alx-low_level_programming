7523acaf552e0ca59a393553dd746913251964c9
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-14 00:41:16 +0000

    gcc 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*Main -Main entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
