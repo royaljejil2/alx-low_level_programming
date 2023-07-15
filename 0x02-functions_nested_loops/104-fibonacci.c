737b7814290ae447dd23a3a14e91c91366463c07
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-15 20:00:23 +0000

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 /**
- *  main - Prints the first 98 Fibonacci numbers
- *
- *  Return: Always 0.
+ * main - Prints the first 98  Fibonacci numbers
+ * Return: Always 0
  */
-
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	int c, bool1, bool2;
+	long int n1 = 1, n2 = 2, fn, fn2, n11, n22;
 
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
+	bool1 = bool2 = 1;
 	printf("%ld, %ld", n1, n2);
 	for (c = 0; c < 96; c++)
 	{
-		if (boolean)
+		if (bool1)
 		{
 			fn = n1 + n2;
 			printf(", %ld", fn);
@@ -25,28 +21,25 @@ int main(void)
 		}
 		else
 		{
-			if (boolean2)
+			if (bool2)
 			{
 				n11 = n1 % 1000000000;
 				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				boolean2 = 0;
+				n1 /= 1000000000;
+				bool2 = 0;
 			}
 			else
 			{
 				fn2 = (n11 + n22);
 				fn = n1 + n2 + (fn2 / 1000000000);
-				printf(", %ld", fn);
-				printf(", %ld", fn2 % 1000000000);
+				printf(", %ld, %ld", fn, fn2 % 1000000000);
 				n1 = n2;
-				n11 = n22;
 				n2 = fn;
 				n22 = (fn2 % 1000000000);
 			}
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		bool1 = !bool1;
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
