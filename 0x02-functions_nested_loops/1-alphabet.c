a26920cae5a2a322f7f06cd70952adb57db5cb2d
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-15 05:59:12 +0000

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * Description: "Write a function that prints the alphabet, in lowercase, followed by a new line."
  * Return: void
  */
 
-void print_alphabet (void)
+void print_alphabet(void)
 {
 	char ch;
 	for (ch = 'a'; ch <='z';ch++)
