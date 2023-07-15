4b4573fb3dc66a645ebd48f33ebea1163761ea0c
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-15 10:19:33 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds two values
+ * @d1: The first number to be computed
+ * @d2: The second number to be computed
+ * Return: Integer value
+ */
+
+int add(int d1, int d2)
+{
+	return (d1 + d2);
+}
