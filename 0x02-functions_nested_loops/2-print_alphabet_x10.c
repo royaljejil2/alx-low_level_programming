e4a15da1acadd576e0d7f8468828005ec05ae158
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-15 07:48:59 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet _x10 - Prints the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	int m;
+
+	for (n = 1; n <= 10; n++)
+	{
+		for (m = 97; m <= 122; m++)
+		{
+			_putchar(m);
+		}
+		_putchar('\n');
+	}
+}
