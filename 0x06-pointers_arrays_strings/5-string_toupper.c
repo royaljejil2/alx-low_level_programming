680eca65c5bbe725cb5ba6f0d8da7ab3b3f7a806
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-07-26 22:10:06 +0000

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *string_toupper - unction that changes all lowercase letters of a string to uppercase.
+ * @v: pointer to string
+ * Return: pointer to uppercase string.
+ */
+
+char *string_toupper(char *v)
+{
+	int jey;
+	jey = 0;
+	while (v[jey] != '\0')
+	{
+		if (v[jey] >= 97 && v[jey] <= 122)
+		{
+			v[jey] = v[jey] - 32;
+		}
+		jey++;
+	}
+	return (v);
+}
