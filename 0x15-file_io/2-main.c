d0c2d875cc1e4b322fef0d1a07fd5f8598a9fbc8
JESSE NII AYERTEY ADJAR <royaljejil2@yahoo.com>
2023-08-24 01:29:59 +0000

    Write a function that appends text at the end of a file.

--- 0x15-file_io/2-main.c
+++ 0x15-file_io/2-main.c
@@ -13,8 +13,8 @@ int main(int ac, char **av)
 
     if (ac != 3)
     {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
+	    dprintf(2, "Usage: %s filename text\n", av[0]);
+	    exit(1);
     }
     res = append_text_to_file(av[1], av[2]);
     printf("-> %i)\n", res);
